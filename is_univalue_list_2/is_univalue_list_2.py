14fa5d7e69396996aad88802ad9324e480c69b43
19Ahmed83 <spb6926@gmail.com>
2026-05-08 22:34:48 +0200

    solution without an array

---
+++ is_univalue_list_2/is_univalue_list_2.py
@@ -0,0 +1,24 @@
+class Node():
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+a = Node(3)
+b = Node(2)
+c = Node(3)
+d = Node(3)
+
+a.next = b
+b.next = c
+c.next = d 
+
+def is_univalue_list_2(head):
+    current = head
+    while current is not None:
+        if current.val != head.val:
+            return False
+        current = current.next
+    return True
+
+print(is_univalue_list_2(a))    
+        
