44948c181d665f1508b1b31028062a6d6d779120
19Ahmed83 <spb6926@gmail.com>
2026-05-09 22:53:50 +0200

    insert_node

---
+++ insert_node/insert_node.py
@@ -0,0 +1,37 @@
+class Node():
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+a = Node("a")
+b = Node("d")
+c = Node("t")
+d = Node("f")
+
+a.next = b
+b.next = c
+c.next = d
+
+def insert_node(head, value, index):
+    if index == 0:
+        new_head = Node(value)
+        new_head.next = head
+
+        return new_head
+
+    count = 0
+    current = head
+
+    while current is not None:
+        if count == index - 1:
+            temp = current.next
+            current.next = Node(value)
+            current.next.next = temp
+
+        count += 1
+        current = current.next
+
+    return head
+
+print(insert_node(a, "x", 2))       
+
