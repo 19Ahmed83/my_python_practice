45e2185981f0352cc0b2133fd808ad06ff8dfd3f
19Ahmed83 <spb6926@gmail.com>
2026-05-09 22:21:21 +0200

    remove_node

---
+++ remove_node/remove_node.py
@@ -0,0 +1,31 @@
+class Node():
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+def remove_node(head, target_val):
+    if head.val == target_val:
+        return head.next
+    
+    current = head
+    prev = None
+
+    while current is not None:
+        if current.val == target_val:
+            prev.next = current.next
+            break
+        prev = current
+        current = current.next
+
+    return head
+
+print(remove_node(a, c))    
