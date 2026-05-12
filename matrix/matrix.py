c17d9a9566f716766fade129b2abf78cc163f5b8
19Ahmed83 <spb6926@gmail.com>
2026-05-12 12:36:28 +0200

    matrix

---
+++ matrix/matrix.py
@@ -0,0 +1,21 @@
+import time, random, sys
+
+width = 70
+
+try:
+    columns = [0] * width
+    while True:
+        for i in range(width):
+            if random.random() < 0.02:
+                columns[i] = random.randint(4, 14)
+            
+            if columns[i] == 0:
+                print(" ", end = "")
+            else:
+                print(random.choice([0, 1]), end = "")
+                columns[i] -= 1
+
+        print() 
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    sys.exit()                       
