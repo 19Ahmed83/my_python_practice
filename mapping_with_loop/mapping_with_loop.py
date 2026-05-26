9bf5678110d482367ce2df43e2b86b7b8171dfb2
19Ahmed83 <spb6926@gmail.com>
2026-05-26 22:52:56 +0200

    mapping_with_loop

---
+++ mapping_with_loop/mapping_with_loop.py
@@ -0,0 +1,9 @@
+names = ["Ali", "John", "Mike", "Bob"]
+numbers = ["0428", "0674", "0184", "0918"]
+
+result = {}
+
+for i in range(len(names)):
+    result[names[i]] =  numbers[i]
+
+print(result)    
